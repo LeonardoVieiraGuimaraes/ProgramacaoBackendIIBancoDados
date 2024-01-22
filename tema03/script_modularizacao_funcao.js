835c056175bc87c27cefcc90e56f56e04dab13c6
leona <leonardovieiraxy@hotmail.com>
2024-01-22 15:09:14 -0300

    Changes to be committed: 	modified:   tema03/aulasNode/script_modularizacao_modulo.js 	modified:   tema03/script_modularizacao_funcao.js 	modified:   tema03/script_vetores_metodos.js

--- tema03/script_modularizacao_funcao.js
+++ tema03/script_modularizacao_funcao.js
@@ -1,4 +1,11 @@
+// Ela consiste em criar módulos, ou seja, funções, para executar diferentes tarefas, as quais podem ser reutilizadas posteriormente em outros códigos, bastando chamá-las.
+
 // Declara uma função chamada 'soma' que recebe dois parâmetros: 'num1' e 'num2'
+
+// Uma função é como um programa separado do programa principal, mas que é feito dentro do próprio código e pode ser chamada nele mesmo.
+
+// As funções também podem ser reutilizadas, ou seja, chamadas várias vezes no mesmo projeto. Se você quiser usar uma função em outro projeto, precisará entrar no código onde ela foi feita para copiá-la.
+
 function soma(num1, num2) {
   // A função retorna a soma de 'num1' e 'num2'
   return num1 + num2;
