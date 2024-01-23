b68f8861664021d841d49d258ae7f10ca56d0a10
LEONARDO VIEIRA GUIMARAES <leonardovieiraxy@hotmail.com>
2024-01-23 01:02:13 -0300

    Changes to be committed: 	renamed:    tema03/script_vetores_metodos.js -> tema03/01_vetores_metodos.js 	renamed:    tema03/script_modularizacao_funcao.js -> tema03/02_modularizacao_funcao.js 	renamed:    tema03/script_numerosAleatorios.js -> tema03/03_numeros_aleatorios.js 	renamed:    tema03/aulasNode/comandos.txt -> tema03/comandos.txt 	renamed:    tema03/aulasNode/main.js -> tema03/main.js 	renamed:    tema03/aulasNode/script_modularizacao_modulo.js -> tema03/modulo_soma.js

--- tema03/main.js
+++ tema03/main.js
@@ -2,8 +2,9 @@
 // Aqui, estamos importando o módulo 'script_modularizacao_modulo' e atribuindo-o à constante 'somar'.
 
 // Importa o módulo 'script_modularizacao_modulo' e atribui à constante 'somar'
-const somar = require("./script_modularizacao_modulo");
+// const somar = require("./script_modularizacao_modulo");
 
+const somar = require("./modulo_soma");
 // Chama a função 'soma' do módulo 'somar' com os argumentos 5 e 2, e imprime o resultado no console
 console.log(somar.soma(5, 2));
 
--- tema03/modulo_soma.js
+++ tema03/modulo_soma.js
@@ -3,7 +3,12 @@
 
 // O módulo é como uma função, mas possui algumas diferenças. Ele é também um programa separado, usado para executar alguma tarefa específica, mas é preciso que ele seja salvo como um arquivo separado do programa principal. Esse arquivo deve ter um nome que corresponda à sua utilidade, e dentro desse arquivo deve conter as instruções que serão executadas quando ele for chamado.
 // Declara uma função chamada 'soma' que recebe dois parâmetros: 'num1' e 'num2'
+
 exports.soma = function (num1, num2) {
-  // A função retorna a soma de 'num1' e 'num2'
   return num1 + num2;
 };
+
+// export function soma(num1, num2) {
+//   // A função retorna a soma de 'num1' e 'num2'
+//   return num1 + num2;
+// }
