3388bf496a02f62041f673e7f5b11a83dd426d88
leona <leonardovieiraxy@hotmail.com>
2024-02-15 15:22:02 -0300

    Changes to be committed: 	modified:   tema08/prgnode/4-HTTP/TipoContas.html 	modified:   tema08/prgnode/4-HTTP/contas.html 	modified:   tema08/prgnode/5-ModExpress/ServidorExpress.js

--- tema08/prgnode/5-ModExpress/ServidorExpress.js
+++ tema08/prgnode/5-ModExpress/ServidorExpress.js
@@ -1,9 +1,19 @@
-// Importa o módulo externo express
+// Importa o módulo Express.js.
 const express = require("express");
+
+// Cria uma nova aplicação Express.
 const app = express();
+
+// Define um manipulador de rota para solicitações GET para a rota raiz ("/").
+// Quando uma solicitação GET é recebida para a rota raiz, a função de callback é chamada.
+// A função de callback envia a resposta "Sou programador Node.js" para o cliente.
 app.get("/", (req, res) => {
   res.send("Sou programador Node.js");
 });
+
+// Faz a aplicação começar a escutar por conexões na porta 3000.
+// Quando o servidor começa a escutar por conexões, a função de callback é chamada.
+// A função de callback imprime a mensagem "Server running at http://localhost:3000/" no console.
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000/");
 });
