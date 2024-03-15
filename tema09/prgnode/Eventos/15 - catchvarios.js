4266465acec8d8deea8850021f548eb375014173
LEONARDO VIEIRA GUIMARAES <leonardovieiraxy@hotmail.com>
2024-03-15 18:23:06 -0300

    Changes to be committed: 	modified:   tema09/prgnode/Eventos/15 - catchvarios.js

--- tema09/prgnode/Eventos/15 - catchvarios.js
+++ tema09/prgnode/Eventos/15 - catchvarios.js
@@ -1,17 +1,10 @@
-// Cria uma nova Promise.
-// A função executora passada para o construtor da Promise imediatamente lança um erro.
 new Promise((resolve, reject) => {
-  throw new Error("Error");
+  throw new Error("Erro na Promise");
 })
-  // Adiciona um manipulador de rejeição à Promise.
-  // Este manipulador é chamado quando a Promise é rejeitada (neste caso, devido ao erro lançado na função executora).
-  // O manipulador de rejeição também lança um erro.
   .catch((err) => {
-    throw new Error("Error");
+    console.error("Primeiro catch: ", err.message);
+    throw new Error("Erro no primeiro catch");
   })
-  // Adiciona outro manipulador de rejeição à Promise.
-  // Este manipulador é chamado quando a Promise é rejeitada pelo primeiro manipulador de rejeição.
-  // O manipulador de rejeição imprime o erro no console.
   .catch((err) => {
-    console.error(err);
+    console.error("Segundo catch: ", err.message);
   });
