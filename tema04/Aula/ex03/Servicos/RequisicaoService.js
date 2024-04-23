b1d8c2f6a4e0754d2e0d0df3690dac4dd5433bf9
LEONARDO VIEIRA GUIMARAES <leonardovieiraxy@hotmail.com>
2024-04-22 21:10:17 -0300

    Changes to be committed: 	modified:   tema04/Aula/ex03/Servicos/RequisicaoService.js

--- tema04/Aula/ex03/Servicos/RequisicaoService.js
+++ tema04/Aula/ex03/Servicos/RequisicaoService.js
@@ -1,6 +1,6 @@
 // Importando os módulos necessários
-const ConexaoBD = require("../ConexaoBD"); // Importando a classe ConexaoBD do módulo conectaBD
-const Aluno = require("../Aluno"); // Importando a classe Aluno do módulo aluno
+const ConexaoBD = require("../ConexaoBD"); // Importando a classe ConexaoBD do módulo ConexaoBD
+const Aluno = require("../Aluno"); // Importando a classe Aluno do módulo Aluno
 
 // Definindo a classe RequisicaoService
 class RequisicaoService {
@@ -19,14 +19,17 @@ class RequisicaoService {
     // Verificando a URL da requisição
     if (request.url === "/") {
       // Se a URL for "/", enviamos uma mensagem de teste como resposta
+      response.writeHead(200, { "Content-Type": "text/plain" });
       response.write("Olá mundo! Teste funcionando...");
     } else if (request.url === "/retornar-alunos") {
       // Se a URL for "/retornar-alunos", buscamos os alunos no banco de dados e enviamos como resposta
       const aluno = new Aluno();
       const alunos = await aluno.getAlunos(connection);
-      response.write(alunos);
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.write(JSON.stringify(alunos));
     } else if (request.url === "/cadastro") {
       // Se a URL for "/cadastro", enviamos uma mensagem como resposta
+      response.writeHead(200, { "Content-Type": "text/html" });
       response.write("<h1>Meu Cadastro</h1>");
     }
 
