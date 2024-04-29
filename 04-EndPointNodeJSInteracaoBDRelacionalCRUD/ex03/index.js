945d48fced3cde156f8707bc1a96897b6bafb907
leona <leonardovieiraxy@hotmail.com>
2024-04-29 16:02:56 -0300

    Changes to be committed: 	modified:   04-EndPointNodeJSInteracaoBDRelacionalCRUD/ex03/index.js 	new file:   tema05/README.md 	renamed:    04-EndPointNodeJSInteracaoBDRelacionalCRUD/ex04/index.js -> tema05/ex04/index.js 	renamed:    04-EndPointNodeJSInteracaoBDRelacionalCRUD/ex04/package-lock.json -> tema05/ex04/package-lock.json 	renamed:    04-EndPointNodeJSInteracaoBDRelacionalCRUD/ex04/package.json -> tema05/ex04/package.json

--- 04-EndPointNodeJSInteracaoBDRelacionalCRUD/ex03/index.js
+++ 04-EndPointNodeJSInteracaoBDRelacionalCRUD/ex03/index.js
@@ -1,32 +1,39 @@
-// index.js
+// Importando todas as funções do módulo conectaBD.js
 const db = require("./conectaBD.js");
 
+// Função auto-executável assíncrona para lidar com operações assíncronas
 (async () => {
   try {
+    // Estabelecendo conexão com o banco de dados
     await db.conecta();
     console.log("Conexão com o banco de dados estabelecida com sucesso!");
-    // Inserir aluno
+
+    // Criando um objeto aluno para inserção
     const carlos = { nome: "Carlos", turma: "A", telefone: "123456789" };
+
+    // Inserindo o aluno no banco de dados
     await db.inserirAluno(carlos);
     console.log("Aluno inserido com sucesso!");
 
-    // Alterar aluno
+    // Criando um objeto aluno para alteração
     const alunoAlterado = { id: 1, telefone: "987654321" };
+
+    // Alterando o aluno no banco de dados
     await db.alterarAluno(alunoAlterado);
     console.log("Aluno alterado com sucesso!");
 
-    // Excluir aluno
+    // Definindo o id do aluno a ser excluído
     const id = 1;
+
+    // Excluindo o aluno do banco de dados
     await db.excluirAluno(id);
     console.log(`Aluno com id ${id} excluído com sucesso!`);
 
-    // Mostrar todos os alunos
+    // Obtendo todos os alunos do banco de dados
     const alunos = await db.todosAlunos();
     console.log("Alunos no banco de dados:", alunos);
   } catch (err) {
-    console.error(
-      "Falha ao conectar realizar operação no banco de dados:",
-      err
-    );
+    // Lidando com erros
+    console.error("Falha ao realizar operação no banco de dados:", err);
   }
 })();
