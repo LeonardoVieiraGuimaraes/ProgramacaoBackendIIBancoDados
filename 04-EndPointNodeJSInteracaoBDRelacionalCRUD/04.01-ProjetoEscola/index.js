95671fd61190a2ba13f335bc5097ae9cdb6ca7da
LEONARDO VIEIRA GUIMARAES <leonardovieiraxy@hotmail.com>
2024-05-06 20:06:16 -0300

    Changes to be committed: 	modified:   04-EndPointNodeJSInteracaoBDRelacionalCRUD/04.01-ProjetoEscola/index.js 	deleted:    04-EndPointNodeJSInteracaoBDRelacionalCRUD/04.01-ProjetoEscola/package-lock.json 	deleted:    04-EndPointNodeJSInteracaoBDRelacionalCRUD/04.01-ProjetoEscola/package.json 	modified:   04-EndPointNodeJSInteracaoBDRelacionalCRUD/04.02-ProjetoEscola/package-lock.json 	modified:   04-EndPointNodeJSInteracaoBDRelacionalCRUD/04.02-ProjetoEscola/package.json

--- 04-EndPointNodeJSInteracaoBDRelacionalCRUD/04.01-ProjetoEscola/index.js
+++ 04-EndPointNodeJSInteracaoBDRelacionalCRUD/04.01-ProjetoEscola/index.js
@@ -1,7 +1,5 @@
 // Importa o módulo 'http' do Node.js, que fornece funcionalidades para criar servidores HTTP.
 const http = require("http");
-const ConexaoBD = require("./conectaBD");
-const Aluno = require("./classes/Aluno");
 
 // Define a porta e o host que o servidor deve usar.
 const porta = 4123;
@@ -16,7 +14,7 @@ const server = http.createServer((request, response) => {
   // Verifica a URL da solicitação e responde de acordo.
   if (request.url === "/") {
     // Se a URL da solicitação for '/', escreve a string "Ola Mundo! Teste Funcionando..." no corpo da resposta HTTP.
-    response.write("Ola Mundo! Teste Funcionando...");
+    response.write("Olá Mundo! Teste Funcionando...");
   } else if (request.url === "/loja") {
     // Se a URL da solicitação for '/loja', escreve a string "Loja" no corpo da resposta HTTP.
     response.write("<h1>Minha Loja<h1>");
@@ -32,20 +30,5 @@ const server = http.createServer((request, response) => {
 // Faz o servidor começar a escutar por conexões no host e porta especificados.
 // A função passada para 'listen' é chamada uma vez que o servidor esteja pronto para aceitar conexões.
 server.listen(porta, host, () => {
-  console.log("O nosso servidor está rodando!");
-
-  // Cria uma nova instância da classe Aluno.
-  const aluno = new Aluno("João", 30);
-
-  // Cria uma nova instância da classe ConexaoBD.
-  const conexaoBD = new ConexaoBD();
-
-  // Obtém a conexão com o banco de dados.
-  const connection = conexaoBD.getConexao();
-
-  // Busca todos os alunos do banco de dados.
-  aluno.getAlunos(connection);
-
-  // Fecha a conexão com o banco de dados.
-  conexaoBD.fecharConexao(connection);
+  console.log(`O nosso servidor está rodando! http://localhost:${porta}`);
 });
